3ccc13308446ecf285e8b1ac0ad13e2e38ecf0f6
Artiom <Artiom@DESKTOP-RGF1328>
2021-09-05 10:24:49 +0300

    Added generating documentation for .html and .json files

--- src/Generator.java
+++ src/Generator.java
@@ -28,8 +28,9 @@ public class Generator {
 
 	/**
 	 * Generates documentation for JSON Schema
-	 * @throws IOException 	 
-	 * */
+	 * 
+	 * @throws IOException
+	 */
 	public void Generate() throws IOException {
 		getObjects();
 		exportDocumentation();
@@ -55,19 +56,24 @@ public class Generator {
 		JSONTokener tokener = new JSONTokener(inputStream);
 		JSONObject object = new JSONObject(tokener);
 
-		// Declare and initialize objects that will store info we need to get
-		JSONObject properties = object.getJSONObject("properties");
-		JSONObject definitions = object.getJSONObject("definitions");
+		try {
+			// Declare and initialize objects that will store info we need to get
+			JSONObject properties = object.getJSONObject("properties");
+			JSONObject definitions = object.getJSONObject("definitions");
 
-		// Get documentation about properties and definitions
-		getDocumentation(properties);
-		getDocumentation(definitions);
+			// Get documentation about properties and definitions
+			getDocumentation(properties);
+			getDocumentation(definitions);
+		} catch (JSONException ex) {
+			System.out.println("Some key not found");
+			ex.printStackTrace();
+		}
 	}
 
 	/**
 	 * Gets the documentation about an certain object
 	 * 
-	 * @param objects - stores the object we need to get info about
+	 * @param objects - Stores the object we need to get info about
 	 */
 	private void getDocumentation(JSONObject objects) {
 		// String that will store the name of current object we need to get info about
@@ -87,70 +93,111 @@ public class Generator {
 			try {
 				currentObject.getString("description");
 				currentObject.getString("title");
-			
+
 				// Store the documentation about current object in documentation HashMap
-				if(fileManager.getOutputExtension() == ".txt") {
+				if (fileManager.getOutputExtension() == ".txt") {
+					documentation.put(currentObjectName, " Title: " + currentObject.getString("title") + ",\n "
+							+ " Description: " + currentObject.getString("description"));
+				} else if (fileManager.getOutputExtension() == ".json") {
+					documentation.put(currentObjectName, " \"Title\": \"" + currentObject.getString("title") + ",\" \n "
+							+ " \"Description\": \"" + currentObject.getString("description") + "\"");
+				} else {
 					documentation.put(currentObjectName, " Title: " + currentObject.getString("title") + ",\n "
 							+ " Description: " + currentObject.getString("description"));
 				}
-				else if(fileManager.getOutputExtension() == ".json") {
-				
-				}
-				else {
-				
-				}
-			}
-			catch(JSONException ex) {
+			} catch (JSONException ex) {
 				System.out.println("Some key not found");
+				ex.printStackTrace();
 			}
 		}
 	}
-	
+
 	/**
 	 * Exports generated documentation into a file
 	 * 
 	 * @throws IOException
 	 */
 	private void exportDocumentation() throws IOException {
-		if(fileManager.getOutputExtension() == ".txt") {
-			exportDocumentationTXT();
+		// Create a BufferedWriter to write documentation into a file
+		BufferedWriter writer = null;
+
+		// Delete the output file if it already exists
+		if (fileManager.getOutputFile().exists()) {
+			fileManager.getOutputFile().delete();
 		}
-		else if(fileManager.getOutputExtension() == ".json") {
-			
+
+		// Link the BufferedWriter with output file
+		if(fileManager.getOutputFile().toString().contains(fileManager.getOutputExtension())){
+			writer = new BufferedWriter(new FileWriter(fileManager.getOutputFile()));
 		}
 		else {
-			
+			// If output file doesn't have extension then add it
+			writer = new BufferedWriter(new FileWriter(fileManager.getOutputFile() + fileManager.getOutputExtension()));
 		}
+		
+		
+		if (fileManager.getOutputExtension() == ".txt") {
+			exportDocumentationTXT(writer);
+		} else if (fileManager.getOutputExtension() == ".json") {
+			exportDocumentationJSON(writer);
+		} else {
+			exportDocumentationHTML(writer);
+		}
+	}
+
+	/**
+	 * Export generated documentation into a .txt file
+	 * @param writer - BufferedWriter to write documentation into file
+	 * @throws IOException
+	 */
+	private void exportDocumentationTXT(BufferedWriter writer) throws IOException {
+		// Write that this is Generated Documentation and open brackets
+		writer.write("Generated Documentation: { \n");
+
+		// Write documentation into the file
+		for (Map.Entry<String, String> element : documentation.entrySet()) {
+			writer.write("\"" + element.getKey() + "\":{\n " + element.getValue() + "\n");
+		}
+
+		// Write a closing bracket to close Generated Data section
+		writer.write("}");
+
+		// Close the writer
+		writer.close();
 	}
 	
 	/**
-	 * Export generated documentation into a .txt file
+	 * Export generated documentation into a .json file
+	 * @param writer - BufferedWriter to write documentation into file 
 	 * @throws IOException
 	 */
-	private void exportDocumentationTXT() throws IOException {
-		// Create a BufferedWriter to write documentation into a file
-		BufferedWriter writer = null;
+	private void exportDocumentationJSON(BufferedWriter writer) throws IOException {
+		writer.write("\"Generated data\": {\n");
 		
-		// Delete the output file if it already exists
-		if(fileManager.getOutputFile().exists()) {
-			fileManager.getOutputFile().delete();
-		}
-		
-		// Link the BufferedWriter with output file
-		writer = new BufferedWriter(new FileWriter(fileManager.getOutputFile()));
-		
-		// Write that this is Generated Documentation and open brackets
-		writer.write(" Generated Documentation: { \n");
-		
-		// Write documentation into the file
 		for(Map.Entry<String, String> element : documentation.entrySet()) {
-			writer.write("\"" + element.getKey() + "\":{\n " + element.getValue() + "\n");
+			writer.write("\"" + element.getKey() + "\":{ \n " + element.getValue() + "\n");
 		}
 		
-		// Write a closing bracket to close Generated Data section
 		writer.write("}");
 		
-		// Close the writer
+		writer.close();
+	}
+	
+	/**
+	 * Export generated documentation into a .html file
+	 * @param writer - BufferedWriter to write documentation into file
+	 * @throws IOException
+	 */
+	private void exportDocumentationHTML(BufferedWriter writer) throws IOException {
+		writer.write("<!DOCTYPE html>\r\n" + "<html lang=\"en\">\r\n" + "\r\n" + "<head>\r\n"
+				+ "<title>Generate JSON Schema Documentation</title>\r\n" + "</head>\r\n" + "\r\n" + "<body>");
+		
+		for(Map.Entry<String, String> element : documentation.entrySet()) {
+			writer.write("<h3>" + element.getKey() + " : </h3> \n " + " <h4>" + element.getValue() + "</h4>\n");
+		}
+		
+		writer.write("</body>\r\n" + "</html>");
+		
 		writer.close();
 	}
 }
